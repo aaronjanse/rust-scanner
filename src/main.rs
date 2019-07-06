36a3d83d16ac7c4c2ea0c2cd12b749eb8b7f6dfa
Aaron Janse <aaron@ajanse.me>
2019-07-05 18:08:58 -0700

    basic scanning

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,192 @@
 fn main() {
-    let text = "Hello, world!";
-    parse(text);
+//   let text = r#"
+// render-firefox $ win = -- Shhh, don't tell the compiler about this
+//   let
+//     close-button = button $ {x: 3, y: 7, w: 2, h: 2}
+//   in
+//   if close-button.closed then
+//     empty-window -- implementation not shown
+//   else
+//     yield win
+//       |> draw-button close-button
+//       |> draw-welcome-screen
+//     render-firefox $ win
+
+
+// button $ props =
+//   {props | closed = wait-click $ props}
+
+
+// wait-click $ {x, y, w, h} =
+//   if $.mouse.down
+//      && props.x <= $.mouse.x < props.x + props.w
+//      && props.y <= $.mouse.y < props.y + props.h
+//   then
+//     true
+//   else
+//     yield false
+//     wait-click $ {x, y, w, h}
+//   "#;
+  let text = String::from("true");
+  let mut tokens: Vec<Token> = Vec::new();
+  ParserIter::init(&text).parse(&mut tokens);
+  println!("{:?}", tokens);
 }
 
-fn parse(text: &str) {
-  let mut iter = TextIter::init(text);
-  loop {
-    match iter.next() {
-      Some(c) => print!("{}", c),
-      None => break,
-    }
-  }
-}
-
-struct TextIter<'a> {
+struct ParserIter<'a> {
+  source: &'a String,
   iter: core::iter::Peekable<core::str::Chars<'a>>,
   next: Option<char>,
+  buffer: String,
+  line: usize,
 }
 
-impl<'a> TextIter<'a> {
-  fn init(s: &'a str) -> TextIter<'a> {
-    let mut iter = s.chars().peekable();
+impl<'a> ParserIter<'a> {
+  fn init(source: &'a String) -> ParserIter<'a> {
+    let mut iter = source.chars().peekable();
     let next = iter.next();
-    TextIter {
-      iter, next,
+    ParserIter {
+      source, iter, next,
+      buffer: String::from(""),
+      line: 0,
     }
   }
-  fn next (&mut self) -> Option<char> {
+
+  fn parse(&mut self, tokens: &mut Vec<Token>) {
+    let mut error: Option<String> = None;
+
+    use crate::TokenType::*;
+    loop {
+      let tok = match self.next() {
+        None => Eof,
+        Some(c) => match c {
+          '\n' | '\r' | '\t' | ' ' => Ignore,
+
+          '(' => LeftParen,
+          ')' => RightParen,
+          '[' => LeftSquareBrace,
+          ']' => RightSquareBrace,
+          '{' => LeftCurlyBrace,
+          '}' => RightCurlyBrace,
+          ':' => Colon,
+          '+' => Plus,
+
+          '!' => match self.peek() {
+            Some('=') => {self.advance(); NotEqual},
+            _ => Unrecognized,
+          },
+          '=' => match self.peek() {
+            Some('=') => {self.advance(); EqualEqual},
+            _ => Equal,
+          },
+          '|' => match self.peek() {
+            Some('|') => {self.advance(); Or},
+            Some('>') => {self.advance(); PipeForwards},
+            _ => Pipe,
+          },
+          '-' => match self.peek() {
+            Some('>') => {self.advance(); Arrow},
+            Some('-') => {
+              loop {
+                match self.next() {
+                  None => break,
+                  Some('\n') => break,
+                  _ => (),
+                };
+              }
+              Comment
+            }
+            _ => Minus,
+          },
+          '<' => match self.peek() {
+            Some('=') => {self.advance(); LessEq},
+            _ => Less,
+          }
+          '>' => match self.peek() {
+            Some('=') => {self.advance(); GreaterEq},
+            _ => Greater,
+          },
+
+          '0'..='9' => {
+            loop {
+              match self.peek() {
+                None => break,
+                Some(c) => match c {
+                  '0'..='9' => self.advance(),
+                  '.' => {
+                    self.advance();
+                    loop {
+                      match self.peek() {
+                        None => break,
+                        Some(c) => match c {
+                          '0'..='9' => self.advance(),
+                          _ => {
+                            error = Some(format!("Invalid char {:?}", c));
+                            break
+                          },
+                        }
+                      }
+                    }
+                    break
+                  },
+                  _ => {
+                    error = Some(format!("Invalid char {:?}", c));
+                    break
+                  },
+                }
+              }
+            };
+            Number
+          },
+
+          _ => Unrecognized,
+        },
+      };
+      match error {
+        None => {
+          match tok {
+            Eof => break,
+            Ignore => self.buffer = String::from(""),
+            _ => self.add_token(tokens, tok),
+          }
+        },
+        Some(err) => {
+          println!("Error on line {}:", self.line);
+          let lines: Vec<&str> = self.source.lines().collect();
+          println!("{}", lines[self.line]);
+          println!("\n{}", err);
+          break
+        },
+      }
+    }
+  }
+
+  fn add_token(&mut self, tokens: &mut Vec<Token>, kind: TokenType) {
+    tokens.push(Token {
+      kind,
+      line: self.line,
+      literal: self.buffer.clone(),
+    });
+    self.buffer = String::from("")
+  }
+
+  fn next(&mut self) -> Option<char> {
     let cur = self.next;
-    self.next = self.iter.next();
-    cur
+    match cur {
+      None => None,
+      Some(c) => {
+        self.buffer.push(c);
+        if c == '\n' {
+          self.line += 1;
+        }
+        self.next = self.iter.next();
+        Some(c)
+      }
+    }
+  }
+
+  fn advance(&mut self) {
+    self.next();
   }
 
   fn peek(&self) -> Option<char> {
@@ -44,35 +201,43 @@ impl<'a> TextIter<'a> {
   }
 }
 
-// struct Token {
-//   kind: TokenType,
-//   line: u32,
-// }
+#[derive(Debug)]
+struct Token {
+  kind: TokenType,
+  line: usize,
+  literal: String,
+}
 
-// enum TokenType {
-//   // Single-character tokens.
-//   LeftParen, RightParen,
-//   LeftBrace, RightBrace,
-//   Colon, Pipe, Backslash,
-//   Plus, Minus, Star, Slash,
-//   Dollar,
+#[derive(Debug)]
+enum TokenType {
+  Unrecognized,
 
-//   // One or two character tokens.
-//   Equal, NotEqual,
-//   And, Or,
-//   Greater, GreaterEq,
-//   Less, LessEq,
-//   Arrow, // ->
-//   PipeForwards,  // |>
+  // Single-character tokens.
+  LeftParen, RightParen,
+  LeftSquareBrace, RightSquareBrace,
+  LeftCurlyBrace, RightCurlyBrace,
+  Colon, Pipe, Backslash,
+  Plus, Minus, Star, Slash,
+  Dollar,
 
-//   // Literals.
-//   Identifier, Str, Number,
+  // One or two character tokens.
+  Equal, EqualEqual, NotEqual,
+  And, Or,
+  Greater, GreaterEq,
+  Less, LessEq,
+  Arrow, // ->
+  PipeForwards,  // |>
 
-//   // Keywords.
-//   True, False,
-//   If, Then, Else,
-//   Let, In,
-//   Yield,
+  // Literals.
+  Identifier, Str, Number,
 
-//   Eof,
-// }
+  // Keywords.
+  True, False,
+  If, Then, Else,
+  Let, In,
+  Yield,
+
+  Comment,
+  Ignore,
+  Eof,
+}
